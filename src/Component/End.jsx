7ea253f02a512db117a5a0c83f66e7bc508c38bb
akilasonti <akilasonti9@gmail.com>
2023-01-15 21:26:46 +0530

    project create

---
+++ src/Component/End.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function End() {
+  return <div>Game Over</div>;
+}
+
+export default End;
---
+++ src/Component/Winner.jsx
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../App";
+
+function Winner() {
+  const { winner } = useContext(AppContext);
+
+  return (
+    <div>
+      Congratulations <span>{winner}</span>, you WON!
+    </div>
+  );
+}
+
+export default Winner;
